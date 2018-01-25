61213f656fc038f452b27d7ae6501b1578f68939
YehEmily <emilyyeh1997@gmail.com>
2018-01-25 15:47:21 -0500

    Adding refactored cards

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,37 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
+
+void getCardName(char *prompt, char *card_name) {
+	puts(prompt);
+	scanf("%2s", card_name);
+}
+
+int getIncrement(int val) {
+	if ((val > 2) && (val < 7)) {
+		return 1;
+	} else if (val == 10) {
+		return -1;
+	} else {
+		return 0;
+	}
+}
+
+void printTotal(int total) {
+	printf("Current count: %i\n", total);
+}
+
+int play() {
 	char card_name[3];
 	int count = 0;
- 	do {
- 		puts("Enter the card_name: ");
-		scanf("%2s", card_name);
+	do {
+		getCardName("Enter the card_name: ", card_name);
 		int val = 0;
 		switch(card_name[0]) {
-		case 'K':
-		case 'Q':
-		case 'J':
-			val = 10;
-			break;
-		case 'A':
-			val = 11;
-			break;
-		case 'X':
-			continue;
-		default:
-			val = atoi(card_name);
-			if ((val < 1) || (val > 10)) {
-				puts("I don't understand that value!");
+			case 'K':
+			case 'Q':
+			case 'J':
+				val = 10;
+				break;
+			case 'A':
+				val = 11;
+				break;
+			case 'X':
 				continue;
-			}
+			default:
+				val = atoi(card_name);
+				if ((val < 1) || (val > 10)) {
+					puts("I don't understand that value!");
+					continue;
+				}
 		}
-		if ((val > 2) && (val < 7)) {
-			count++;
-		} else if (val == 10) {
-			count--;
-		}
-		printf("Current count: %i\n", count);
-	} while (card_name[0] != 'X')
-		return 0;
+		printTotal(count + getIncrement(val));
+	} while (card_name[0] != 'X');
+	return 0;
+}
+
+int main() {
+	return play();
 }
