5bb54bea8a77287e444a4b00164306bd77e1d437
YehEmily <emilyyeh1997@gmail.com>
2018-01-25 15:54:48 -0500

    Adding final cards file with comments and everything

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,11 +1,27 @@
+/*
+ * Exercise 01 by Emily Yeh for SoftSys 2018
+ * This file helps a player count the cards played in a game of blackjack.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * Prompts the user for the name of the card that has been played.
+ * 
+ * prompt: Message to display
+ * card_Name: User-input result
+ */
 void getCardName(char *prompt, char *card_name) {
 	puts(prompt);
 	scanf("%2s", card_name);
 }
 
+/*
+ * Returns the value by which the running total should increase.
+ * 
+ * val: The number value of the card that has been played
+ */
 int getIncrement(int val) {
 	if ((val > 2) && (val < 7)) {
 		return 1;
@@ -16,10 +32,18 @@ int getIncrement(int val) {
 	}
 }
 
-void printTotal(int total) {
-	printf("Current count: %i\n", total);
+/*
+ * Prints the total of the cards played so far.
+ * 
+ * total: The current total
+ */
+void printTotal(int current) {
+	printf("Current count: %i\n", current);
 }
 
+/*
+ * Simulates a game of blackjack.
+ */
 int play() {
 	char card_name[3];
 	int count = 0;
@@ -49,6 +73,9 @@ int play() {
 	return 0;
 }
 
+/*
+ * The main function.
+ */
 int main() {
 	return play();
 }
