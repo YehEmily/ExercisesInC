a5a60a304ee6637e7790943d17c0d5522ca61e90
YehEmily <emilyyeh1997@gmail.com>
2018-02-02 11:11:35 -0500

    Checking in stack.c

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -12,9 +12,9 @@ License: GNU GPLv3
 
 int *foo() {
     int i;
-    int array[SIZE];
+    int array[SIZE]; // Local array
 
-    //printf("%p\n", array);
+//    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = 42;
@@ -24,9 +24,9 @@ int *foo() {
 
 void bar() {
     int i;
-    int array[SIZE];
+    int array[SIZE]; // Also local array
 
-    //printf("%p\n", array);
+//    printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
         array[i] = i;
@@ -36,12 +36,19 @@ void bar() {
 int main()
 {
     int i;
-    int *array = foo();
+    int *array = foo(); // Trying to assign a pointer to a function's local array
     bar();
 
     for (i=0; i<SIZE; i++) {
-        printf("%d\n", array[i]);
+        printf("%d\n", array[i]); // Likely where the segmentation fault occurs
     }
 
     return 0;
 }
+
+/* The arrays initialized in `foo()` and then `bar()` are
+ * both located at the same address.
+ * This is because both arrays were local variables that
+ * were only stored in the stack while their respective
+ * functions were running.
+ */
