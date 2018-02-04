c212f23e45ddae119214f429f473ae91ef711887
YehEmily <emilyyeh1997@gmail.com>
2018-02-04 12:27:01 -0500

    Updating exercises

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -10,6 +10,11 @@ License: GNU GPLv3
 
 int var1;
 
+void printLocalVariable() {
+	int a = 0;
+	printf("Address of local variable a is %p\n", &a);
+}
+
 int main ()
 {
     int var2 = 5;
@@ -20,7 +25,18 @@ int main ()
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("Address of p is %p\n", p);
-    printf ("Address of s is %p\n", s);
+    printf ("Address of s is %p\n\n", s);
 
+    p = malloc(10);
+	printf ("Address of p is now %p\n\n", p);
+	
+	printLocalVariable();
+	
+	void *b = malloc(10);
+	void *c = malloc(10);
+	
+	printf ("Address of b is %p\n", b);
+	printf ("Address of c is %p\n\n", c);
+	
     return 0;
 }
